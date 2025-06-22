385cfa645275aeced31a3bf9ce0d5aa2eb7870b3
SebastienGras <sebastien.gras75@gmail.com>
2025-06-22 14:27:41 +0200

    fix(axios): get bearer token function

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 
+const getBearerToken = (): string => {
+  const user = localStorage.getItem("user");
+  if (user) {
+    const parsedUser = JSON.parse(user);
+    return parsedUser.token || "";
+  }
+  return "";
+};
+
 export const api = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
-    Authorization: `Bearer ${
-      JSON.parse(localStorage.getItem("user") || "{}")?.token || ""
-    }`,
+    Authorization: `Bearer ${getBearerToken()}`,
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
